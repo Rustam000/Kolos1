dd1ce6801e252d1fee22417fa4c309698f32eb4e
Maxim <334890@gmail.com>
2023-10-16 13:58:07 +0600

    стилизовал страницу Карточка дистрибьютора

--- src/pages/DistributorProfile/DistributorProfile.jsx
+++ src/pages/DistributorProfile/DistributorProfile.jsx
@@ -2,6 +2,8 @@ import BasicTable from "../../components/MUITable/MUITable";
 import { products } from "../../components/MUITable/beer_data";
 import Button from "../../components/UI/Button/Button";
 import styles from "./DistributorProfile.module.css";
+import angleBracketLeftIcon from "../../assets/icons/fi-sr-angle-small-left.svg";
+import editIcon from "../../assets/icons/edit.svg";
 
 const tableColumns = [
   {
@@ -46,34 +48,58 @@ const tableColumns = [
 export default function DistributorProfile() {
   const total = products?.reduce(
     (acc, item) => acc + item.price * item.quantity,
-    0
+    0,
   );
   return (
     <div className={styles.DistributorProfile}>
       <div className="container">
-        <p>{"< Отменить"}</p>
+        <p className={styles.undo}>
+          <img src={angleBracketLeftIcon} alt="icon" />
+          Отменить
+        </p>
         <h1 className={styles.heading}>Карточка дистрибьютора</h1>
-        <main>
+        <main className={styles.mainSection}>
           <div className={styles.infoBlock}>
-            <img className={styles.photo} src="" alt="" />
-            <div className={styles.info}>
-              <p className={styles.name}>name</p>
-              <p className={styles.region}>region</p>
-              <p className={styles.inn}>inn</p>
-              <p className={styles.phone}>phone</p>
-              <p className={styles.phone}>phone</p>
-              <div className={styles.controls}>
+            <img
+              className={styles.photo}
+              src="/temporary_distributor_image.png"
+              alt="фото дистрибьютора"
+              width={196}
+            />
+            <div className={styles.infoRows}>
+              <p className="infoRow">
+                <span className={styles.infoRowLabel}>ФИО:</span>Баланчаев
+                Баланча баланчаевич
+              </p>
+              <p className="infoRow">
+                <span className={styles.infoRowLabel}>Регион:</span>Чуй
+              </p>
+              <p className="infoRow">
+                <span className={styles.infoRowLabel}>ИНН:</span>22708198700000
+              </p>
+              <p className="infoRow">
+                <span className={styles.infoRowLabel}>Контактный номер:</span>
+                +996 550 366 000
+              </p>
+              <p className="infoRow">
+                <span className={styles.infoRowLabel}>Контактный номер:</span>
+                +996 550 366 001
+              </p>
+              <div className={styles.actions}>
                 <Button variant="secondary">Возврат</Button>
                 <Button variant="secondary">Продать</Button>
                 <Button variant="primary" width="narrow">
-                  edit
+                  <img src={editIcon} alt="edit icon" />
                 </Button>
               </div>
             </div>
           </div>
-          <form className={styles.filterBar}>
-            <input type="text" />
-            <label>
+          <form className={styles.filterbar}>
+            <input type="search" placeholder="Поиск..." />
+            <select name="" id="">
+              <option value="all">Все товары</option>
+            </select>
+            <label className={styles.filterbarDate}>
               <span>Дата</span>
               <input type="date" />
             </label>
@@ -82,7 +108,10 @@ export default function DistributorProfile() {
           <div className={styles.nav}>
             <Button variant="primary">Список история продаж</Button>
             <Button variant="secondary">Список история возврата</Button>
-            <span className={styles.total}>Итого:{total}</span>
+            <span className={styles.total}>
+              {"Итого: "}
+              {total}
+            </span>
           </div>
         </main>
       </div>
