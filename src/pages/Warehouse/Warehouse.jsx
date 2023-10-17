d31c6fc8a89d4ffb2d6b45480878946fd37589e2
Maxim <334890@gmail.com>
2023-10-17 11:03:14 +0600

    добавил элементы управления в страницу склад

--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import BasicTable from "../../components/MUITable/MUITable";
 import { products } from "../../components/MUITable/beer_data";
+import Button from "../../components/UI/Button/Button";
 import styles from "./Warehouse.module.css";
 
 const columns = [
@@ -43,10 +45,24 @@ const columns = [
 ];
 
 export default function Warehouse() {
+  const navigate = useNavigate();
   return (
     <div className={styles.Warehouse}>
       <div className="container">
-        <h1 style={{ textAlign: "center" }}>Страница Склад</h1>
+        {/* <h1 style={{ textAlign: "center" }}>Страница Склад</h1> */}
+        <div className={styles.filterbar}>
+          <input type="search" placeholder="Поиск..." />
+          <select name="" id="">
+            <option value="all">Все товары</option>
+          </select>
+          <select name="" id="">
+            <option value="all">Норма</option>
+          </select>
+          <Button variant="secondary">Архив</Button>
+          <Button variant="primary" onClick={() => navigate("/create-product")}>
+            Создать
+          </Button>
+        </div>
         <BasicTable data={products} columns={columns} />
       </div>
     </div>
