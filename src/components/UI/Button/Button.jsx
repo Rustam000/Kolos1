2d662c7b0308757a2f506ef2642dc6caeb20af7e
Maxim <334890@gmail.com>
2023-10-18 08:43:05 +0600

    add jsdoc to Button component

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,9 +1,24 @@
 import styles from "./Button.module.css";
 
+/**
+ * @typedef {'narrow' | 'wide' | 'full'} ButtonWidth
+ * @typedef {'primary' | 'secondary'} ButtonVariant
+ *
+ * @param {Object} param0
+ * @param {import("react").ReactNode} param0.children Button text or icon
+ * @param {ButtonWidth} [param0.width="wide"] wide|narrow|full
+ * @param {ButtonVariant} [param0.variant="primary"] primary|secondary
+ * @param {boolean} [param0.disabled=false]
+ * @param {()=>unknown|undefined} [param0.onClick=undefined]
+ *
+ * @description width can be: wide|narrow|full
+ * @description variant can be: primary|secondary
+ *
+ */
 export default function Button({
   children,
-  width = "wide",
-  variant = "primary",
+  width = "wide", //wide|narrow|full
+  variant = "primary", //primary|secondary
   disabled = false,
   onClick = undefined,
 }) {
