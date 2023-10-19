f0a29f32d090e02dab11fe803c6b384605989f3e
Maxim <334890@gmail.com>
2023-10-19 10:13:21 +0600

    adjust disabled button colors

--- src/components/UI/CustomButton/CustomButton.jsx
+++ src/components/UI/CustomButton/CustomButton.jsx
@@ -6,18 +6,18 @@ import styles from "./CustomButton.module.css";
  *
  * @param {Object} param0
  * @param {import("react").ReactNode} param0.children Button text or icon
- * @param {ButtonWidth} [param0.width="wide"] wide|narrow|full
+ * @param {ButtonWidth} [param0.width="wide"] narrow|medium|wide|full
  * @param {ButtonVariant} [param0.variant="primary"] primary|secondary
  * @param {boolean} [param0.disabled=false]
  * @param {()=>unknown|undefined} [param0.onClick=undefined]
  *
- * @description width can be: wide|narrow|full
+ * @description width can be: narrow|medium|wide|full
  * @description variant can be: primary|secondary
  *
  */
 export default function CustomButton({
   children,
-  width = "wide", //wide|narrow|full
+  width = "wide", //narrow|medium|wide|full
   variant = "primary", //primary|secondary
   disabled = false,
   onClick = undefined,
