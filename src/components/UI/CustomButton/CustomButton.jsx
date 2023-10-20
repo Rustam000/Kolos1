ac33902f51489276f7d0976ecea0a95b2b5eabcf
Maxim <334890@gmail.com>
2023-10-20 17:51:26 +0600

    add flex button width

--- src/components/UI/CustomButton/CustomButton.jsx
+++ src/components/UI/CustomButton/CustomButton.jsx
@@ -7,19 +7,19 @@ import styles from "./CustomButton.module.css";
  *
  * @param {Object} param0
  * @param {import("react").ReactNode} param0.children Button text or icon
- * @param {ButtonWidth} [param0.width="wide"] narrow|medium|wide|full
+ * @param {ButtonWidth} [param0.width="wide"] narrow|medium|wide|full|flex
  * @param {ButtonVariant} [param0.variant="primary"] primary|secondary
- * @param {ButtonType} [param0.type="submit"] primary|secondary
+ * @param {ButtonType} [param0.type="submit"]
  * @param {boolean} [param0.disabled=false]
  * @param {()=>unknown|undefined} [param0.onClick=undefined]
  *
- * @description width can be: narrow|medium|wide|full
+ * @description width can be: narrow|medium|wide|full|flex
  * @description variant can be: primary|secondary
  *
  */
 export default function CustomButton({
   children,
-  width = "wide", //narrow|medium|wide|full
+  width = "wide", //narrow|medium|wide|full|flex
   variant = "primary", //primary|secondary
   disabled = false,
   type = "submit",
