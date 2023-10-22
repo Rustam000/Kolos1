46913e2d9a75ea8feedaec093bac32529064795b
Maxim <334890@gmail.com>
2023-10-22 14:51:40 +0600

    add edit links and dynamic routes

--- src/pages/Distributors/Distributors.jsx
+++ src/pages/Distributors/Distributors.jsx
@@ -6,44 +6,49 @@ import { Table } from "antd";
 import TableButton from "../../components/UI/TableButton/TableButton";
 import editIcon from "../../assets/icons/mode_edit.svg";
 
-const tableColumns = [
-  {
-    title: "№",
-    dataIndex: "rowIndex",
-    key: "rowIndex",
-    align: "center",
-    width: 50,
-    render: (text, record, index) => index + 1, // автоматическое нумерование
-  },
-  {
-    title: "ФИО",
-    dataIndex: "name",
-    key: "name",
-    align: "left",
-    width: 350,
-  },
-  {
-    title: "Регион",
-    dataIndex: "region",
-    key: "region",
-    align: "left",
-    width: 325,
-  },
-  {
-    title: "Ред.",
-    key: "action",
-    align: "center",
-    width: 78,
-    render: (_, record) => (
-      <TableButton onClick={() => console.log(record._id)}>
-        <img src={editIcon} alt="edit icon" />
-      </TableButton>
-    ),
-  },
-];
-
 export default function Distributors() {
   const navigate = useNavigate();
+
+  const tableColumns = [
+    {
+      title: "№",
+      dataIndex: "rowIndex",
+      key: "rowIndex",
+      align: "center",
+      width: 50,
+      render: (text, record, index) => index + 1, // автоматическое нумерование
+    },
+    {
+      title: "ФИО",
+      dataIndex: "name",
+      key: "name",
+      align: "left",
+      width: 350,
+    },
+    {
+      title: "Регион",
+      dataIndex: "region",
+      key: "region",
+      align: "left",
+      width: 325,
+    },
+    {
+      title: "Ред.",
+      key: "action",
+      align: "center",
+      width: 78,
+      render: (_, record) => (
+        <TableButton
+          onClick={() =>
+            navigate(`/edit-distributor/${record._id}`, { state: record })
+          }
+        >
+          <img src={editIcon} alt="edit icon" />
+        </TableButton>
+      ),
+    },
+  ];
+
   return (
     <div className={styles.Distributors}>
       <div className="container">
--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import CustomTable from "../../components/CustomTable/CustomTable";
 import { products } from "../../assets/beer_data";
 import CustomButton from "../../components/UI/CustomButton/CustomButton";
 import styles from "./Warehouse.module.css";
@@ -8,65 +7,70 @@ import editIcon from "../../assets/icons/mode_edit.svg";
 import { Table } from "antd";
 import TableButton from "../../components/UI/TableButton/TableButton";
 
-const tableColumns = [
-  {
-    title: "№",
-    dataIndex: "rowIndex",
-    key: "rowIndex",
-    align: "center",
-    width: 50,
-    render: (text, record, index) => index + 1, // автоматическое нумерование
-  },
-  {
-    title: "Наименование",
-    dataIndex: "name",
-    key: "name",
-    align: "left",
-    width: 350,
-  },
-  {
-    title: "Уникальный код",
-    dataIndex: "num_id",
-    key: "num_id",
-    align: "left",
-    width: 325,
-  },
-  {
-    title: "Ед. изменения.",
-    dataIndex: "unit",
-    key: "unit",
-    align: "left",
-    width: 150,
-  },
-  {
-    title: "Кол-во",
-    dataIndex: "quantity",
-    key: "quantity",
-    align: "left",
-    width: 125,
-  },
-  {
-    title: "Цена",
-    dataIndex: "price",
-    key: "price",
-    align: "left",
-    width: 125,
-  },
-  {
-    title: "Ред.",
-    key: "action",
-    align: "center",
-    width: 78,
-    render: (_, record) => (
-      <TableButton onClick={() => console.log(record._id)}>
-        <img src={editIcon} alt="edit icon" />
-      </TableButton>
-    ),
-  },
-];
-
 export default function Warehouse() {
   const navigate = useNavigate();
+
+  const tableColumns = [
+    {
+      title: "№",
+      dataIndex: "rowIndex",
+      key: "rowIndex",
+      align: "center",
+      width: 50,
+      render: (text, record, index) => index + 1, // автоматическое нумерование
+    },
+    {
+      title: "Наименование",
+      dataIndex: "name",
+      key: "name",
+      align: "left",
+      width: 350,
+    },
+    {
+      title: "Уникальный код",
+      dataIndex: "num_id",
+      key: "num_id",
+      align: "left",
+      width: 325,
+    },
+    {
+      title: "Ед. изменения.",
+      dataIndex: "unit",
+      key: "unit",
+      align: "left",
+      width: 150,
+    },
+    {
+      title: "Кол-во",
+      dataIndex: "quantity",
+      key: "quantity",
+      align: "left",
+      width: 125,
+    },
+    {
+      title: "Цена",
+      dataIndex: "price",
+      key: "price",
+      align: "left",
+      width: 125,
+    },
+    {
+      title: "Ред.",
+      key: "action",
+      align: "center",
+      width: 78,
+      render: (_, record) => (
+        <TableButton
+          onClick={() =>
+            navigate(`/edit-product/${record._id}`, { state: record })
+          }
+        >
+          <img src={editIcon} alt="edit icon" />
+        </TableButton>
+      ),
+    },
+  ];
+
   return (
     <div className={styles.Warehouse}>
       <div className="container">
