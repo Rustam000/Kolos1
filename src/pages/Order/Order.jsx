9d4ddd2a2a7d6e36dc76a26440afa2fb7fea31b6
arlen isaev <arlenisaev43@gmail.com>
2023-10-23 17:06:34 +0600

    replaced MUI table with Antd table

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,75 +1,109 @@
 import styles from "./Order.module.css";
-import { products } from "../../components/CustomTable/beer_data";
-import CustomTable from "../../components/CustomTable/CustomTable";
-
-const columns = [
-  {
-    dataKey: "rowIndex",
-    label: "№",
-    align: "left",
-  },
-  {
-    dataKey: "name",
-    label: "Наименование",
-    align: "left",
-  },
-  {
-    dataKey: "num_id",
-    label: "Уникальный код",
-    align: "left",
-  },
-  {
-    dataKey: "unit",
-    label: "Ед. изм.",
-    align: "left",
-  },
-  {
-    dataKey: "quantity",
-    label: "Кол-во",
-    align: "left",
-  },
-  {
-    dataKey: "price",
-    label: "Цена",
-    align: "left",
-  },
-  {
-    dataKey: "action",
-    label: "Ред.",
-    align: "left",
-    onClick: (dataKey) => console.log(dataKey),
-    icon: "icon",
-  },
-];
+import { useNavigate } from "react-router-dom";
+import editIcon from "../../assets/icons/Vector.svg"
+import { Table } from "antd";
+import TableButton from "../../components/UI/TableButton/TableButton";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchItems } from "../../redux/warehouseSlice";
+import searchIcon from "../../assets/icons/search.svg";
 
 export default function Order() {
+  const { products } = useSelector((state) => state.warehouse);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchItems());
+  }, []);
+
+  const tableColumns = [
+    {
+      title: "№",
+      dataIndex: "rowIndex",
+      key: "rowIndex",
+      align: "center",
+      width: 50,
+      render: (text, record, index) => index + 1, // автоматическое нумерование
+    },
+    {
+      title: "Наименование",
+      dataIndex: "name",
+      key: "name",
+      align: "left",
+      width: 350,
+    },
+    {
+      title: "Уникальный код",
+      dataIndex: "num_id",
+      key: "num_id",
+      align: "left",
+      width: 325,
+    },
+    {
+      title: "Ед. изм.",
+      dataIndex: "unit",
+      key: "unit",
+      align: "left",
+      width: "12%",
+    },
+    {
+      title: "Кол-во",
+      dataIndex: "quantity",
+      key: "quantity",
+      align: "left",
+      width: "12%",
+    },
+    {
+      title: "Цена",
+      dataIndex: "price",
+      key: "price",
+      align: "left",
+      width: "12%",
+    },
+    { 
+      title: "Дублировать",
+      key: "action",
+      align: "center",
+      width: 78,
+      render: (_, record) => (
+        <TableButton
+          onClick={() =>
+            navigate(`/edit-product/${record._id}`, { state: record })
+          }
+        >
+          <img src={editIcon} alt="edit icon" />
+        </TableButton>
+      ),
+    },
+  ];
   return (
     <div className={styles.Order}>
       <input
         className={styles.search}
         type="search"
         placeholder="Поиск..."
-      ></input>
+      />
       <div className={styles.box}>
         <div className={styles.dis}>
           <div className={styles.destre}>
             <div className={styles.bgimg}></div>
             <div className={styles.info}>
-              <p style={{ marginTop: "10px" }}>
+              <p>
                 <span className={styles.infoRow}>ФИО:</span>Баланчаев Баланча
                 баланчаевич
               </p>
-              <p style={{ marginTop: "10px" }}>
+              <p>
                 <span className={styles.infoRow}>Регион:</span>Чуй
               </p>
-              <p style={{ marginTop: "10px" }}>
+              <p>
                 <span className={styles.infoRow}>ИНН:</span>22708198700000
               </p>
-              <p style={{ marginTop: "10px" }}>
+              <p>
                 <span className={styles.infoRow}>Контактный номер:</span>
                 +996 550 366 000
               </p>
-              <p style={{ marginTop: "10px" }}>
+              <p>
                 <span className={styles.infoRow}>Контактный номер:</span>
                 +996 550 366 001
               </p>
@@ -81,11 +115,22 @@ export default function Order() {
           </div>
         </div>
         <div className={styles.prod}>
-          <h2>Товар со склада </h2>
-          <CustomTable data={products} columns={columns} />
-          <p className={styles.result}>
-            Итог: <h2 style={{ margin: "-7px 10px" }}>9999</h2>
-          </p>
+          <h2 className={styles.product}>Товар со склада </h2>
+          <div className={styles.table}>
+            <Table
+              bordered
+              dataSource={products}
+              rowKey="_id"
+              columns={tableColumns}
+              pagination={false}
+              scroll={{y: "45vh", scrollToFirstRowOnChange: true}}
+            />
+          </div>
+          <div className={styles.result}>
+            <span>
+              Итог: <h2>9999</h2>
+            </span>
+          </div>
         </div>
       </div>
       <div className={styles.btn}>
