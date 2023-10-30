a9229d4f7f4d9a7cbbb8f16287321c2bc2ffbb1f
Maxim <334890@gmail.com>
2023-10-30 10:26:44 +0600

    fetch from kolos api & show loading state

--- src/redux/distributorsSlice.js
+++ src/redux/distributorsSlice.js
@@ -6,6 +6,20 @@ export const fetchDistributors = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await axios.get(
+        `http://51.20.115.221/api/v1/distributor/`,
+      );
+      return response.data.results;
+    } catch (error) {
+      console.warn(error);
+    }
+  },
+);
+/* 
+export const fetchDistributors = createAsyncThunk(
+  "distributors/fetchDistributors",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(
         `https://jwt-authentication-beryl.vercel.app/api/distributors`,
       );
       return response.data;
@@ -13,10 +27,12 @@ export const fetchDistributors = createAsyncThunk(
       console.warn(error);
     }
   },
-);
+); */
 
 const initialState = {
   distributors: [],
+  isLoading: false,
+  error: null,
 };
 
 export const distributorsSlice = createSlice({
@@ -24,8 +40,18 @@ export const distributorsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    builder.addCase(fetchDistributors.pending, (state, action) => {
+      state.error = null;
+      state.isLoading = true;
+    });
     builder.addCase(fetchDistributors.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.error = null;
       state.distributors = action.payload;
+    });
+    builder.addCase(fetchDistributors.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
     });
   },
 });
