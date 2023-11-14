7724820fe0bd38d7ee8c98732b3e50ffc4ad0fce
Maxim <334890@gmail.com>
2023-11-14 16:59:11 +0600

    improve yearLimiter

--- src/utils/yearLimiter.js
+++ src/utils/yearLimiter.js
@@ -3,6 +3,7 @@
  * @description takes YYYYYY-MM-DD string and trims excess year digits
  */
 export default function yearLimiter(dateString) {
+  if (dateString === "") return "";
   const [year, month, day] = dateString.split("-");
   const fourDigitYear = year.slice(0, 4);
   const correctDateString = [fourDigitYear, month, day].join("-");
